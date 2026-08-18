ba600b44b92322cd4e34529d1338cdd69043af79
SteveMLC <121320326+SteveMLC@users.noreply.github.com>
2026-08-18 11:33:00 -0400

    fix: load GA4 on static export

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,5 +1,3 @@
-import Script from "next/script";
-
 const MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
 export function GoogleAnalytics() {
@@ -9,18 +7,20 @@ export function GoogleAnalytics() {
 
   return (
     <>
-      <Script
+      <script
+        async
         src={`https://www.googletagmanager.com/gtag/js?id=${MEASUREMENT_ID}`}
-        strategy="afterInteractive"
       />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${MEASUREMENT_ID}');
-        `}
-      </Script>
+        `,
+        }}
+      />
     </>
   );
 }
